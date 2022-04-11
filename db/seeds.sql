ead20691af17235d8d9c709edc667f093a609978
Tony Taylor <73037339+Crimsonsurf07@users.noreply.github.com>
2022-04-11 18:12:15 -0400

    destinations table created along with db.sql, schema for destinations table, and insert statement seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,14 +3,7 @@ VALUES ('Ringo', 'Starr', 'ringostarr@gmail.com', 'password2');
 
 -- // INSERT statement for destinations TABLE
 INSERT INTO destinations (id, city, country_state, activities, recommendations, dates_travelled)
-
-VALUES
-   ('1',
-   'Las Vegas',
-    'USA_Nevada',
-    'Sight Seeing, Music Shows, Las Vegas Strip, Amazing Food',
-    'If Possible Avoid Gambling',
-    '7');
+VALUES('1','Las Vegas','USA_Nevada','Sight Seeing, Music Shows, Las Vegas Strip, Amazing Food','If Possible Avoid Gambling','7');
 
 
 -- Populate The Table with data entered here
