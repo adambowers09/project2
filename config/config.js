40ac776e4ebfc55303ddef438f55b15ba14ad304
Adam Bowers <adambowers09@live.com>
2022-04-12 20:56:00 -0400

    jawsdb

--- config/config.js
+++ config/config.js
@@ -1,23 +1,27 @@
-module.exports = {
-  development: {
-    username: process.env.SEQUELIZE_USER,
-    password: process.env.SEQUELIZE_PASSWORD,
-    database: 'project2_dev',
-    dialect: 'mysql',
-    host: process.env.SEQUELIZE_HOST,
-    port: 3306
-  },
-  test: {
-    username: process.env.TU,
-    password: process.env.TP || null,
-    database: 'project2_test',
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql',
-    logging: false
-  },
-  production: {
-    'use_env_variable': 'JAWSDB_URL',
-    dialect: 'mysql'
-  }
-};
+const Sequelize = require("sequelize");
+
+// send config to .env to get the db connection variables
+require("dotenv").config();
+
+// use a variable that can be reassigned (ie let)
+let sequelize;
+// if statement to use Heroku JawsDB or local if not on Heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      // username: "root",
+      // password: "password",
+      // database: "project2_dev",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
